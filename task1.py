05149fdd9377a1e9f0e3173c5097c0b1f5c73af1
funnemonkee <ghostsmans@gmail.com>
2023-04-26 10:39:48 +0300

    Падает на 23 тесте от времени

--- task1.py
+++ task1.py
@@ -3,17 +3,17 @@ class Datacenter:
         self.r = 0
         self.m = m
         self.a = m
-        self.state = 2 ** m - 1
+        self.state = [True for i in range(m)]
 
     def reset(self):
         self.r += 1
-        self.state = 2 ** self.m - 1
+        self.state = [True for i in range(self.m)]
         self.a = self.m
 
     def disable(self, to_disable: int):
-        state_of_to_disable = int(bin(self.state)[to_disable + 2])
+        state_of_to_disable = self.state[to_disable]
         if state_of_to_disable:
-            self.state -= 2 ** to_disable
+            self.state[to_disable] = False
             self.a -= 1
 
     def get_val(self) -> int:
@@ -39,19 +39,24 @@ def getminind(dc_arr: list[Datacenter], n: int) -> int:
 
 if __name__ == "__main__":
     n, m, q = [int(i) for i in input().split()]
+    min_ind = max_ind = 0
     dc_arr = [Datacenter(m) for i in range(n)]
     for i in range(q):
         operation = input().split()
         if operation[0] == 'GETMAX':
-            print(getmaxind(dc_arr, n))
+            print(max_ind)
             continue
         elif operation[0] == 'GETMIN':
-            print(getminind(dc_arr, n))
+            print(min_ind)
             continue
         dc_index = int(operation[1]) - 1
         if operation[0] == 'RESET':
             dc_arr[dc_index].reset()
+            if dc_arr[dc_index].get_val() > max_ind:
+                max_ind = dc_index
             continue
         elif operation[0] == 'DISABLE':
             server_index = int(operation[2]) - 1
             dc_arr[dc_index].disable(server_index)
+            if dc_arr[dc_index].get_val() < min_ind:
+                min_ind = dc_index
